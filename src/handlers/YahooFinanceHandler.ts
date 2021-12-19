02c9247a4f30a954aa786e77b3d32eb1d9eb2250
Ryunhee Han <ryuneeee@gmail.com>
2021-12-20 06:35:22 +0900

    fix husky

--- src/handlers/YahooFinanceHandler.ts
+++ src/handlers/YahooFinanceHandler.ts
@@ -8,22 +8,26 @@ export class YahooFinanceHandler implements MessageHandler {
   handle(msg: Message) {
     let delimeter = msg.text().substring(0, 1);
     let code = msg.text().substring(1);
-    let tickers = code.split(',')
-    
+    let tickers = code.split(',');
+
     if (delimeter !== '$') return;
-    
-    axios.get(`${FINANCE_HOST}/price/${code}`).then(function (response: AxiosResponse) {
-      let reply: string = ""
 
-      tickers.map(t => t.trim()).map(ticker => {
-        let price = response.data[ticker].regularMarketPrice;
-        reply += `> ${ticker}: $${price}\n`  
+    axios
+      .get(`${FINANCE_HOST}/price/${code}`)
+      .then(function (response: AxiosResponse) {
+        let reply: string = '';
+
+        tickers
+          .map((t) => t.trim())
+          .map((ticker) => {
+            let price = response.data[ticker].regularMarketPrice;
+            reply += `> ${ticker}: $${price}\n`;
+          });
+
+        msg.reply(reply);
       })
-
-      msg.reply(reply);
-
-    }).catch(function (error) {
-      console.error(error);
-    })
+      .catch(function (error) {
+        console.error(error);
+      });
   }
 }
